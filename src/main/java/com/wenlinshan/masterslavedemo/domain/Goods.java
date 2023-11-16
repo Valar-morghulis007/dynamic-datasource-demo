3ccb84d664e3eea77603824a734070f9200419a4
Valar-morghulis007 <1261644617@qq.com>
2023-11-17 00:51:11 +0800

    初始化项目

---
+++ src/main/java/com/wenlinshan/masterslavedemo/domain/Goods.java
@@ -0,0 +1,30 @@
+package com.wenlinshan.masterslavedemo.domain;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@TableName(value = "goods")
+public class Goods {
+    @TableId(value = "id", type = IdType.ASSIGN_ID)
+    private Long id;
+
+    @TableField(value = "`name`")
+    private String name;
+
+    @TableField(value = "quantity")
+    private Integer quantity;
+
+    @TableField(value = "price")
+    private BigDecimal price;
+}
