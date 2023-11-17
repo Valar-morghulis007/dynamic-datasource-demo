5bf901246cd14a9df3a24d4deb5b7611626f7e34
Valar-morghulis007 <1261644617@qq.com>
2023-11-18 00:56:53 +0800

    初始化项目

--- src/main/java/com/morghulis/masterslavedemo/annotation/Master.java
+++
@@ -1,7 +0,0 @@
-package com.morghulis.masterslavedemo.annotation;
-
-/**
- * @author wls
- */
-public @interface Master {
-}
--- src/main/java/com/morghulis/masterslavedemo/mapper/GoodsMapper.java
+++
@@ -1,7 +0,0 @@
-package com.morghulis.masterslavedemo.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.morghulis.masterslavedemo.domain.Goods;
-
-public interface GoodsMapper extends BaseMapper<Goods> {
-}
